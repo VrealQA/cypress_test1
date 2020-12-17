a103130a5bc50a9fa21bf23390e1d575f5fc990e
Vreal-QA <vrealqa@gmail.com>
2020-12-17 16:25:40 +0200

    init

---
+++ integration/sample_spec.js
@@ -0,0 +1,7 @@
+describe('M211y First Test', () => {
+    it('Does not do much!', () => {
+        cy.viewport(1280, 720)
+        cy.visit("https://foodfunk.vrealsoft.com/")
+        cy.contains("Μενώ")
+    })
+})
